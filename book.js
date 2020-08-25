cd502dcd627bdf45cbea32f0c495446ba53b304e
WonderNote18 <hector.d.almanza@gmail.com>
2020-08-24 21:46:19 -0500

    Added addBookToLibrary to add entries to myLibrary

--- book.js
+++ book.js
@@ -9,3 +9,14 @@ function Book(title, author, pages, read) {
         return title + ' by ' + author + ', ' + pages + ' pages, ' + read;
     }
 }
+
+function addBookToLibrary() {
+    let userData = {};
+
+    userData.title = window.prompt("Enter your book's title: ");
+    userData.author = window.prompt("Enter the Author of the book: ");
+    userData.pages = window.prompt("Enter the amount of pages in this book: ");
+    userData.read = false;
+    myLibrary.push(userData);
+    alert(`Your book ${userData.title} by ${userData.author} with ${userData.pages} pages has been added.`);
+}
