a4d5b34d4119eb1c7764c03923c5ef1e5ffe06c8
WonderNote18 <hector.d.almanza@gmail.com>
2020-09-01 23:28:56 -0500

    Updated  constructors/functions for updated HTML Added id key to Book contructor used to add values to grid Updated addBookToLibrary to get next book id based on myLibrary length Updated addBookToLibrary to create Book obj before pushing to myLibrary Updated addToGrid to require index of new book for child element ID's Updated addToGrid to create bookContainer div to hold all values in grid Updated addToGrid to only add values from Book's title, author, pages Updated addToGrid to create cells for read status and row actions

--- book.js
+++ book.js
@@ -1,36 +1,68 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return title + ' by ' + author + ', ' + pages + ' pages, ' + read;
+class Book {
+    constructor(id, title, author, pages) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = false;
+    }
+    markAsRead() {
+        this.read = true;
+        document.getElementById("read" + id).setAttribute("class", "gridFont gridReadTrue");
+        document.getElementById("read" + id).textContent = "Completed";
     }
 }
 
-function addBookToLibrary() {
-    let userData = {};
 
-    userData.title = window.prompt("Enter your book's title: ");
-    userData.author = window.prompt("Enter the Author of the book: ");
-    userData.pages = window.prompt("Enter the amount of pages in this book: ");
-    userData.read = false;
-    myLibrary.push(userData);
-    render();
-    alert(`Your book ${userData.title} by ${userData.author} with ${userData.pages} pages has been added.`);
+function addBookToLibrary() {
+    let bookId;
+    myLibrary.length == 0 ? bookId = 0 : bookId = myLibrary.length;
+    let bookTitle = window.prompt("Enter your book's Title");
+    let bookAuthor = window.prompt("Enter the Author of the book");
+    let bookPages = window.prompt("Enter the amount of pages in this book");
+
+    let newBook = new Book(bookId, bookTitle, bookAuthor, bookPages);
+    newBook.prototype = Object.create(Book.prototype);
+    newBook.addToLibrary();
+    myLibrary.push(newBook);
+
+    addToGrid(newBook.id);
+    
+    alert(`Your book ${newBook.title} by ${newBook.author} with ${newBook.pages} pages has been added.`);
 }
 
-function render() {
-    let bookContainer = document.getElementById('bookContainer');
-    bookContainer.innerHTML = "";
-    myLibrary.forEach(function(book) {
-        var bookString = `${book.title} by ${book.author}, ${book.pages} pages.`
-        book.read ? bookString += ' Has read.' : bookString += ' Has not read.';
-        
-        var bookContent = document.createElement("p");
-        bookContent.textContent = bookString
-        bookContainer.appendChild(bookContent);
-    });
+function addToGrid(libraryId) {
+    let bookContainer = document.createElement("div");
+    let bookData = myLibrary[libraryId];
+    bookContainer.setAttribute("id", "bookContainer" + libraryId);
+    bookContainer.setAttribute("class", "gridEntry");
+    document.getElementById("bookGrid").appendChild(bookContainer);
+
+    let dataKeys = Object.keys(bookData);
+    dataKeys.splice(dataKeys.indexOf('id'), 1);
+    dataKeys.splice(dataKeys.indexOf('prototype'), 1);
+    dataKeys.splice(dataKeys.indexOf('read'), 1);
+
+    for (i = 0; i < dataKeys.length; i++) {
+        let dataElement = document.createElement("p");
+        let dataKey = dataKeys[i];
+        dataElement.setAttribute("id", dataKey + libraryId);
+        dataElement.setAttribute("class", "gridFont");
+        dataElement.textContent = bookData[dataKey];
+        bookContainer.appendChild(dataElement);
+    }
+
+    let readElement = document.createElement("p");
+    readElement.setAttribute("id", "read" + libraryId);
+    readElement.setAttribute("class", "gridFont gridReadFalse");
+    readElement.textContent = "Not Completed";
+    bookContainer.appendChild(readElement);
+    
+    let actionElement = document.createElement("div");
+    actionElement.setAttribute("id", "actions" + libraryId);
+    actionElement.setAttribute("class", "gridFont gridActions");
+    actionElement.textContent = "TEMP";
+    bookContainer.appendChild(actionElement);
 }
